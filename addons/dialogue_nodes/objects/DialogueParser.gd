f06cab01439693e31570184ac8cfce09cabf3428
Jim Hessin <jhessin@gmail.com>
2026-09-08 08:47:13 -0600

    Added custom nodes to DialogueParser

--- addons/dialogue_nodes/objects/DialogueParser.gd
+++ addons/dialogue_nodes/objects/DialogueParser.gd
@@ -3,8 +3,8 @@
 @icon('res://addons/dialogue_nodes/icons/DialogueParser.svg')
 class_name DialogueParser
 extends Node
-# TODO : Fix [wait] bbcode
 
+# TODO : Fix [wait] bbcode
 ## Triggered when a dialogue has started. Passes [param id] of the dialogue tree as defined in the StartNode.
 signal dialogue_started(id: String)
 ## Triggered when a single dialogue block has been processed.
@@ -43,9 +43,11 @@ var _nest_links: Array[String] = []
 
 ## Loads the [param DialogueData] resource from the given [param path]. The loaded resource can be accessed using [member data].
 func load_data(path: String) -> void:
-	if not path.ends_with('.tres'): return
+	if not path.ends_with('.tres'):
+		return
 	var new_data := ResourceLoader.load(path, '', ResourceLoader.CACHE_MODE_IGNORE)
-	if not new_data is DialogueData: return
+	if not new_data is DialogueData:
+		return
 	set_data(new_data)
 
 
@@ -58,15 +60,20 @@ func set_data(new_data: DialogueData) -> void:
 	_nest_links.clear()
 	variables.clear()
 	characters.clear()
-	
-	if not data: return
-	
+
+	if not data:
+		return
+
 	if data.get('variables'):
 		for var_name in data.variables:
 			variables[var_name] = data.variables[var_name].value
-	
+
 	if data.characters.ends_with('.tres'):
-		var character_list = ResourceLoader.load(data.characters, '', ResourceLoader.CACHE_MODE_IGNORE)
+		var character_list = ResourceLoader.load(
+			data.characters,
+			'',
+			ResourceLoader.CACHE_MODE_IGNORE,
+		)
 		if character_list is CharacterList:
 			characters = character_list.characters
 
@@ -79,7 +86,7 @@ func start(start_id: String) -> void:
 	if not data.starts.has(start_id):
 		printerr('Start ID ', start_id, ' not found in dialogue data!')
 		return
-	
+
 	_running = true
 	if _nest_links.size() == 0:
 		dialogue_started.emit(start_id)
@@ -94,23 +101,26 @@ func stop() -> void:
 
 ## Continues processing the dialogue tree from the node connected to the option at [param idx].
 func select_option(idx: int) -> void:
-	if not _running: return
-	
+	if not _running:
+		return
+
 	if _option_links.size() == 0 or idx > _option_links.size():
 		printerr('Option idx ', idx, ' is not selectable.')
 		return
-	
+
 	option_selected.emit(idx)
 	_proceed(_option_links[idx])
 
 
 ## Returns [code]true[/code] if the [DialogueParser] is processing a dialogue tree.
-func is_running() -> bool: return _running
+func is_running() -> bool:
+	return _running
 
 
 # Proceeds the parser to the next node and runs its corresponding _process_* function.
 func _proceed(node_name: String) -> void:
-	if not _running: return
+	if not _running:
+		return
 	if node_name == 'END':
 		if _nest_links.size() > 0:
 			# resume from previous data
@@ -120,20 +130,30 @@ func _proceed(node_name: String) -> void:
 		else:
 			stop()
 		return
-	
+
+	var node_data: Dictionary = data.nodes[node_name]
+
+	if node_data.has('type'):
+		var node_type := str(node_data['type'])
+
+		if node_type.begins_with('custom:'):
+			_process_custom(node_type.trim_prefix('custom:'), node_data)
+			return
+
 	var process_functions := [
 		_process_start,
 		_process_dialogue,
-		func(): pass, # comment
+		func():
+			pass, # comment
 		_process_signal,
 		_process_set,
 		_process_condition,
 		_process_nest,
-		_process_fork
+		_process_fork,
 	]
-	
+
 	var id := int(node_name.split('_')[0])
-	
+
 	process_functions[id].call(data.nodes[node_name])
 
 
@@ -145,25 +165,28 @@ func _process_start(dict: Dictionary) -> void:
 # Processes the dialogue node data (dict).
 func _process_dialogue(dict: Dictionary) -> void:
 	var speaker = ''
-	
+
 	if dict.speaker is String:
 		speaker = dict.speaker
 	elif dict.speaker is int and characters.size() > 0 and dict.speaker < characters.size():
 		speaker = characters[dict.speaker]
-	
+
 	var dialogue_text = _parse_variables(dict.dialogue)
 	dialogue_text = tr(dialogue_text)
-	
+
 	var option_texts: Array[String] = []
 	_option_links.clear()
 	for option in dict.options.values():
-		if option.condition.is_empty() or _check_condition(option.condition) or skip_options_condition_checks:
+		if (
+			option.condition.is_empty() or _check_condition(option.condition)
+			or skip_options_condition_checks
+		):
 			option_texts.append(_parse_variables(option.text))
 			_option_links.append(option.link)
 	if option_texts.size() == 0:
 		option_texts.append('')
 		_option_links.append('END')
-	
+
 	dialogue_processed.emit(speaker, dialogue_text, option_texts)
 
 
@@ -179,11 +202,11 @@ func _process_set(dict: Dictionary) -> void:
 		printerr('Variable ', dict.variable, ' not found in variables list')
 		_proceed(dict.link)
 		return
-	
+
 	var type = typeof(variables[dict.variable])
 	var value = dict.value
 	var operator = dict.type
-	
+
 	# set datatype of value
 	match typeof(variables[dict.variable]):
 		TYPE_STRING:
@@ -219,7 +242,7 @@ func _process_set(dict: Dictionary) -> void:
 			variables[dict.variable] *= value
 		4:
 			variables[dict.variable] /= value
-	
+
 	variable_changed.emit(dict.variable, variables[dict.variable])
 	_proceed(dict.link)
 
@@ -231,7 +254,7 @@ func _process_condition(dict: Dictionary) -> void:
 
 
 # Processes the fork node data (dict).
-func _process_fork(dict : Dictionary) -> void:
+func _process_fork(dict: Dictionary) -> void:
 	var result = dict.default
 	var forks = dict.forks
 	# index traversal to ensure they're checked in order
@@ -246,53 +269,63 @@ func _process_fork(dict : Dictionary) -> void:
 func _check_condition(conditions: Array) -> bool:
 	var result := true
 	var combiner := 1
-	
+
 	for dict: Dictionary in conditions:
-		if dict.is_empty(): continue
-		
+		if dict.is_empty():
+			continue
+
 		var value1 = dict.value1
 		var value2 = dict.value2
-		
+
 		# get variables if needed
 		if value1.count('{{') > 0:
 			value1 = _parse_variables(value1)
 		if value2.count('{{') > 0:
 			value2 = _parse_variables(value2)
-		
+
 		# evaluate values if neither values contain any alphabets (otherwise treat them as strings)
 		var regex := RegEx.new()
 		regex.compile('[a-zA-Z]+')
 		if not regex.search(value1) and not regex.search(value2):
 			var expression := Expression.new()
-			
+
 			if expression.parse(value1) != OK:
 				printerr(expression.get_error_text(), ' ', value1)
 				return false
 			value1 = expression.execute()
-			
+
 			if expression.parse(value2) != OK:
 				printerr(expression.get_error_text(), ' ', value2)
 				return false
 			value2 = expression.execute()
-		
+
 		# perform operation
-		var cur_result : bool
+		var cur_result: bool
 		match dict.operator:
-			0: cur_result = value1 == value2
-			1: cur_result = value1 != value2
-			2: cur_result = value1 > value2
-			3: cur_result = value1 < value2
-			4: cur_result = value1 >= value2
-			5: cur_result = value1 <= value2
-			_: cur_result = false
-		
+			0:
+				cur_result = value1 == value2
+			1:
+				cur_result = value1 != value2
+			2:
+				cur_result = value1 > value2
+			3:
+				cur_result = value1 < value2
+			4:
+				cur_result = value1 >= value2
+			5:
+				cur_result = value1 <= value2
+			_:
+				cur_result = false
+
 		# combine results using AND/OR
 		match combiner:
-			0: result = result or cur_result
-			1: result = result and cur_result
+			0:
+				result = result or cur_result
+			1:
+				result = result and cur_result
 		if dict.has('combiner'):
 			combiner = dict.combiner
-	
+
 	return result
 
 
@@ -302,7 +335,7 @@ func _process_nest(dict: Dictionary) -> void:
 		printerr('Invalid file: ', dict.file_path)
 		_proceed(dict.link)
 		return
-	
+
 	var new_data := ResourceLoader.load(dict.file_path, '', ResourceLoader.CACHE_MODE_IGNORE)
 	if not new_data is DialogueData:
 		printerr('Invalid resource type: resource must be DialogueData')
@@ -312,18 +345,33 @@ func _process_nest(dict: Dictionary) -> void:
 	_characters.push_back(characters.duplicate())
 	_nest_links.push_back(dict.link)
 	data = new_data
-	
+
 	for var_name in data.variables:
-		if variables.has(var_name): continue
+		if variables.has(var_name):
+			continue
 		variables[var_name] = data.variables[var_name].value
-	
+
 	characters.clear()
 	if data.characters.ends_with('.tres'):
-		var character_list = ResourceLoader.load(data.characters, '', ResourceLoader.CACHE_MODE_IGNORE)
+		var character_list = ResourceLoader.load(
+			data.characters,
+			'',
+			ResourceLoader.CACHE_MODE_IGNORE,
+		)
 		if character_list is CharacterList:
 			characters = character_list.characters
-	
+
 	start(dict.start_id)
+
+
+func _process_custom(id: String, node_data: Dictionary) -> void:
+	var processor := DialogueNodes.get_processor(StringName(id))
+
+	if not processor.is_valid():
+		push_error("DialogueParser: No processor registered for custom node '%s'." % id)
+		return
+
+	processor.call(node_data, self)
 
 
 # Replaces all {{}} variables with their corresponding values in the value string.
@@ -333,21 +381,21 @@ func _parse_variables(value: String) -> String:
 	if value.count('{{') != value.count('}}'):
 		printerr('Failed to parse variables. Missing {{ or }}.')
 		return value
-	
+
 	# format floats to display properly
-	var formatted_variables := {}
+	var formatted_variables := { }
 	for key in variables.keys():
 		if variables[key] is float:
 			formatted_variables[key] = '%0.2f' % variables[key]
 		else:
 			formatted_variables[key] = variables[key]
-	
+
 	# add invalid variables as '' in formatted_variables
 	for key in _parse_variable_names(value):
 		if not variables.has(key):
 			printerr('Unknown variable ', key, ' in string.')
 			formatted_variables[key] = ''
-	
+
 	return value.format(formatted_variables, '{{_}}')
 
 
@@ -356,7 +404,10 @@ func _parse_variable_names(value: String) -> Array:
 	var regex := RegEx.new()
 	regex.compile('{{([^{}]+)}}')
 	var results = regex.search_all(value)
-	results = results.map(func(val): return val.get_string(1))
+	results = results.map(
+		func(val):
+			return val.get_string(1),
+	)
 	return results
 
 
@@ -366,17 +417,17 @@ func _update_wait_tags(node: RichTextLabel, value: String) -> String:
 	# Empty string freezes dialogues
 	if value == '':
 		value = ' '
-	
+
 	# add a wait if none present at beginning
 	if not value.begins_with('[wait'):
 		value = '[wait]' + value + '[/wait]'
-	
+
 	# find the actual position of the last character sans bbcode
 	value = value.replace('\n', ' ').replace('[br]', '\n')
 	node.text = value
-	
+
 	var text_length := node.get_parsed_text().length() - value.count('\n')
-	
+
 	# update [wait] with last attribute for showing options
 	var idx := 0
 	var char_idx := -1
@@ -388,20 +439,22 @@ func _update_wait_tags(node: RichTextLabel, value: String) -> String:
 				var open_tag_start := value.findn('[wait', idx)
 				var open_tag_end := value.findn(']', idx)
 				var end_tag := value.findn('[/wait]', idx)
-				
+
 				var img_tag := value.findn('[img', idx)
 				var img_tag_end := value.findn('[/img]', idx)
-				
+
 				var lb_tag := value.findn('[lb]', idx)
 				var rb_tag := value.findn('[rb]', idx)
-				
+
 				if open_tag_start == idx:
 					var start_idx := char_idx + 1
 					waits.push_back({ 'at': open_tag_end, 'start': start_idx })
 					idx = open_tag_end + 1
 				elif end_tag == idx:
 					var start_data: Dictionary = waits.pop_back()
-					var insert_text := ' start='+str(start_data.start)+' last='+str(start_data.last)+' length='+str(text_length)
+					var insert_text := ' start=' + str(start_data.start) + ' last=' + str(
+						start_data.last
+					) + ' length=' + str(text_length)
 					value = value.insert(start_data.at, insert_text)
 					idx = end_tag + insert_text.length() + 7
 				elif img_tag == idx:
@@ -422,11 +475,13 @@ func _update_wait_tags(node: RichTextLabel, value: String) -> String:
 				char_count += 1
 				if waits.size():
 					waits[-1]['last'] = char_count - 1
-	
+
 	# insert waits if any left
 	while len(waits) > 0:
 		var start_data: Dictionary = waits.pop_back()
-		var insert_text := ' start='+str(start_data.start)+' last='+str(char_count - 1)+' length='+str(text_length)
+		var insert_text := ' start=' + str(start_data.start) + ' last=' + str(char_count - 1) + ' length=' + str(
+			text_length
+		)
 		value = value.insert(start_data.at, insert_text)
-	
+
 	return value
