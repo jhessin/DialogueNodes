f4d183ea199e7b774235ad8cfe293a861cc22fbd
Jim Hessin <jhessin@gmail.com>
2026-09-09 21:25:00 -0600

    Added Array[CustomNode] to DialogueData

--- addons/dialogue_nodes/objects/DialogueData.gd
+++ addons/dialogue_nodes/objects/DialogueData.gd
@@ -16,3 +16,12 @@ extends Resource
 @export var strays: Array[String] = []
 ## Contains the characters available to dialogue nodes.
 @export var characters: CharacterList
+## Contains the custom nodes to be used in this dialogue if any.
+@export var custom_nodes: Array[CustomNode]
+
+var custom_node_dict: Dictionary[StringName, CustomNode]:
+	get:
+		var result: Dictionary[StringName, CustomNode] = { }
+		for node in custom_nodes:
+			result[node.id] = node
+		return result
